f82cba02528af47f783012237f91d938a4a8cc22
TriAgency <phil@triagency.com>
2014-12-03 00:38:23 -0500

    #5 - trying out another method to use newer uploader

--- mp3-to-post.php
+++ mp3-to-post.php
@@ -76,39 +76,12 @@ function mp3_admin() {
     // end POST check
     ?>
     <hr />
-    <script language="JavaScript">
-        jQuery(document).ready(function($){
-        	var _custom_media = true,
-        	_orig_send_attachment = wp.media.editor.send.attachment;
 
-        	$('#_unique_name_button').click(function(e) {
-        		var send_attachment_bkp = wp.media.editor.send.attachment;
-        		var button = $(this);
-        		var id = button.attr('id').replace('_button', '');
-        		_custom_media = true;
-        		wp.media.editor.send.attachment = function(props, attachment){
-        			if ( _custom_media ) {
-        				$("#"+id).val(attachment.url);
-        			} else {
-        				return _orig_send_attachment.apply( this, [props, attachment] );
-        			};
-        		}
-
-        		wp.media.editor.open(button);
-        		return false;
-        	});
-
-        	$('.add_media').on('click', function(){
-        		_custom_media = false;
-        	});
-        });
-    </script>
-
-        <div class="uploader">
-          <input id="upload_image" type="text" size="36" name="ad_image" value="" />
-          <br />
-          <input id="upload_image_button" class="button-primary" type="button" value="Upload" />
-        </div>
+    <div class="uploader">
+      <input id="upload_image" type="text" size="36" name="ad_image" value="" />
+      <br />
+      <input id="upload_image_button" class="button-primary" type="button" value="Upload" />
+    </div>
 
   </div>
 <?php
