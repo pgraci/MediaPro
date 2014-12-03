8fa4df9fd21e72178491cdd0e6565ce21677271b
TriAgency <phil@triagency.com>
2014-12-02 23:43:58 -0500

    #5 - trying out another method to use newer uploader

--- mp3-to-post.php
+++ mp3-to-post.php
@@ -77,50 +77,37 @@ function mp3_admin() {
     ?>
     <hr />
     <script language="JavaScript">
-        jQuery(document).ready(function() {
+        jQuery(document).ready(function($){
+        	var _custom_media = true,
+        	_orig_send_attachment = wp.media.editor.send.attachment;
 
-          var file_frame;
+        	$('#_unique_name_button').click(function(e) {
+        		var send_attachment_bkp = wp.media.editor.send.attachment;
+        		var button = $(this);
+        		var id = button.attr('id').replace('_button', '');
+        		_custom_media = true;
+        		wp.media.editor.send.attachment = function(props, attachment){
+        			if ( _custom_media ) {
+        				$("#"+id).val(attachment.url);
+        			} else {
+        				return _orig_send_attachment.apply( this, [props, attachment] );
+        			};
+        		}
 
-            jQuery('#upload_image_button').on('click', function( event ){
+        		wp.media.editor.open(button);
+        		return false;
+        	});
 
-              event.preventDefault();
-
-              // If the media frame already exists, reopen it.
-              if ( file_frame ) {
-                file_frame.open();
-                return;
-              }
-
-              // Create the media frame.
-              file_frame = wp.media.frames.file_frame = wp.media({
-                title: jQuery( this ).data( 'uploader_title' ),
-                button: {
-                  text: jQuery( this ).data( 'uploader_button_text' ),
-                },
-                multiple: false  // Set to true to allow multiple files to be selected
-              });
-
-              // When an image is selected, run a callback.
-              file_frame.on( 'select', function() {
-                // We set multiple to false so only get one image from the uploader
-                attachment = file_frame.state().get('selection').first().toJSON();
-
-                // Do something with attachment.id and/or attachment.url here
-
-                alert(attachment.url);
-              });
-
-              // Finally, open the modal
-              file_frame.open();
-            });
-
+        	$('.add_media').on('click', function(){
+        		_custom_media = false;
+        	});
         });
     </script>
 
-
-    		<input id="upload_image" type="text" size="36" name="upload_image" value="" />
-        <input id="upload_image_button" class="button-primary" type="button" value="Select Media" />
-
+        <div class="uploader">
+        	<input id="_unique_name" name="settings[_unique_name]" type="text" />
+        	<input id="_unique_name_button" class="button-primary" name="_unique_name_button" type="text" value="Upload or Select Media" />
+        </div>
 
   </div>
 <?php
