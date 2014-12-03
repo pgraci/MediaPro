fc75d0d2de08e2cbe8715a14ef12d86edacbe051
TriAgency <phil@triagency.com>
2014-12-03 01:49:38 -0500

    #5 - trying out another method to use newer uploader

--- my-admin.js
+++ my-admin.js
@@ -25,9 +25,7 @@ jQuery(document).ready(function($){
 
         //When a file is selected, grab the URL and set it as the text field's value
         custom_uploader.on('select', function() {
-            attachment = custom_uploader.state().get('selection').each().toJSON();
-
-
+            attachment = custom_uploader.state().get('selection').first().toJSON();
             alert(attachment.url);
         });
 
