18287deca33802de013c44bc973b92396736d218
TriAgency <phil@triagency.com>
2014-12-02 21:16:25 -0500

    #5 - changed button to use class=button-primary

--- mp3-to-post.php
+++ mp3-to-post.php
@@ -103,8 +103,7 @@ function mp3_admin() {
     <tr valign="top">
     	<td>Upload Image</td>
     	<td><label for="upload_image">
-    		<input id="upload_image" type="text" size="36" name="upload_image" value="<?php echo $gearimage; ?>" />
-    		<input id="upload_image_button" type="button" value="Upload Image" />
+    		<input class="button-primary" type="button" value="Select Media" />
     		<br />Enter an URL or upload an image for the banner.
     		</label>
     	</td>
