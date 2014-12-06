2f4b8a1c9fa40cdbda4eb1c832b37f2b09bf54f5
TriAgency <phil@triagency.com>
2014-12-06 00:48:08 -0500

    #12 - fix issue with individual posts all using same artwork. MASSIVE codebase cleanup

--- mp3-to-post.php
+++ mp3-to-post.php
@@ -153,201 +153,176 @@ function mp3_only($filename) {
  */
 function audio_to_song_post($limit = 'all', $list_of_ids, $folderPath, $urlPath, $post_type, $posting_mode) {
   $messages = array();
-  $mp3Files = array();
-
-  $mp3Files_array = explode(',', $list_of_ids); //split string into array seperated by ', '
-    foreach($mp3Files_array as $song_id) //loop over values
-    {
-
-    // get url of song
-    // get image id of attached featured image
-    // also get the original description and use that if nothing else present
-
-      $song_url = wp_get_attachment_url($song_id);
-      $post_thumbnail_id = get_post_thumbnail_id($song_id);
-
-      // lookup each song's url path by replacing url path with folder path
-        $song_diskpath = str_replace($urlPath,$folderPath,$song_url);
-
-        array_push($mp3Files, $song_diskpath);
-    }
-
-  //sort($mp3Files);
 
   // check of there are files to process
-  if(count($mp3Files) == 0){
+  if(count($list_of_ids) == 0){
     array_push($messages, _e('There are no files to process', 'audio-to-song-post'));
     return $messages;
-  }
-
-  // Initialize getID3 engine
-  $getID3 = new getID3;
-
-  // loop through all the files and create posts
-  $i = 0;
-  if ($posting_mode == '1') {
-    $limit = count($mp3Files) - 1;
   } else {
-    $limit--; // subtract one to work with arrays
+
+      // Initialize getID3 engine
+      $getID3 = new getID3;
+
+      $mp3Files_array = explode(',', $list_of_ids); //split string into array seperated by ', '
+        foreach($mp3Files_array as $song_id) //loop over values
+        {
+
+          // get url of media attachment
+          $song_url = wp_get_attachment_url($song_id);
+
+          // get image id of attached featured image
+          $post_thumbnail_id = get_post_thumbnail_id($song_id);
+
+          // TODO also get the original description and use that if nothing else present
+
+          // lookup each song's url path by replacing url path with folder path
+          $filePath = str_replace($urlPath,$folderPath,$song_url);
+
+          $ThisFileInfo = $getID3->analyze($filePath);
+
+
+              // TODOS
+              // check to see if it has idv2 tags if so use them
+              // if not use id3v1 tags
+              // for itunes purchases, make sure to use other field names
+              // allow user to insert songs / playlist into existing post!
+
+              $title = $ThisFileInfo['tags_html']['id3v2']['title'][0];
+              $album = $ThisFileInfo['tags_html']['id3v2']['album'][0];
+
+              if ($posting_mode == '2') {
+                $title = $album;
+              } else {
+                $title = $title;
+              }
+
+
+
+              $category = $ThisFileInfo['tags_html']['id3v2']['genre'][0];
+              $description = $ThisFileInfo['tags_html']['id3v2']['subtitle'][0];
+              $bpm = $ThisFileInfo['tags_html']['id3v2']['bpm'][0];
+              $composer = $ThisFileInfo['tags_html']['id3v2']['composer'][0];
+              $grouping = $ThisFileInfo['tags_html']['id3v2']['content_group_description'][0];
+              $album_artist = $ThisFileInfo['tags_html']['id3v2']['band'][0];
+              $encoded_by = $ThisFileInfo['tags_html']['id3v2']['encoded_by'][0];
+
+              $comment0 = $ThisFileInfo['tags_html']['id3v2']['comments'][0];
+              $comment1 = $ThisFileInfo['tags_html']['id3v2']['comments'][1];
+              $comment2 = $ThisFileInfo['tags_html']['id3v2']['comments'][2];
+              $comment3 = $ThisFileInfo['tags_html']['id3v2']['comments'][3];
+
+              if (testcommentsforvalid($comment0)) {
+                $comment = $comment0;
+              } elseif (testcommentsforvalid($comment1)) {
+                $comment = $comment1;
+              } elseif (testcommentsforvalid($comment2)) {
+                $comment = $comment2;
+              } elseif (testcommentsforvalid($comment3)) {
+                $comment = $comment3;
+              } else {
+                $comment = "";
+              }
+
+              if ($post_type == 'post') {
+
+                  if ($posting_mode == '1') {
+                    $playlist_ids = $song_id;
+                  } else {
+                    $playlist_ids = $list_of_ids;
+                  }
+
+                  $description = "<p>[playlist ids=" . $playlist_ids . "]</p>" . $description;
+
+              } else {
+                $the_playlist_array = array(
+                  'title' => $title,
+                  'mp3' => $song_url,
+                  'buy_title_a' => '',
+                  'buy_icon_a' => 'cloud-download',
+                  'buy_link_a' => '',
+                  'buy_title_b' => '',
+                  'buy_icon_b' => 'cloud-download',
+                  'buy_link_b' => '',
+                  'buy_title_c' => '',
+                  'buy_icon_c' => 'cloud-download',
+                  'buy_link_c' => '',
+                  'buy_title_d' => '',
+                  'buy_icon_d' => 'cloud-download',
+                  'buy_link_d' => '',
+                );
+
+                //loop thru all files and push each array into array_final key is numeric placement of track
+
+                $the_playlist_array_final = array(
+                  '0' => $the_playlist_array,
+                );
+
+
+              }
+
+              // check if we have a title
+              if ($title){
+
+                // check if post exists by search for one with the same title
+                // filtering by song name not working
+                $searchArgs = array(
+                  'post_title_like' => $title,
+                  'post_type_like' => $post_type,
+                );
+                $titleSearchResult = new WP_Query($searchArgs);
+
+                // If there are no posts with the title of the mp3 then make the post
+                if ($titleSearchResult->post_count == 0) {
+                  // create basic post with info from ID3 details
+                  $my_post = array(
+                    'post_title' => $title,
+                    'post_content' => $description,
+                    'post_author' => 1,
+                    'post_name' => $title,
+                  );
+                  // Insert the post!!
+                  $postID = wp_insert_post($my_post);
+
+                  // set post type
+                  set_post_type($postID, $post_type);
+
+                  if ($post_type == 'songs') {
+                    // TODO set artist for songs posts
+                    add_post_meta($postID, "playlist", $the_playlist_array_final);
+                  }
+
+                  //set post tags
+                  wp_set_post_tags($postID, $comment);
+
+                  //set featured image
+                  set_post_thumbnail($postID, $post_thumbnail_id);
+
+                  // If the category/genre is set then update the post
+                  if(!empty($category)){
+                    $category_ID = get_cat_ID($category);
+                    // if a category exists
+                    if($category_ID) {
+                      $categories_array = array($category_ID);
+                      wp_set_post_categories($postID, $categories_array);
+                    }
+                    // if it doesn't exist then create a new category
+                    else {
+                      $new_category_ID = wp_create_category($category);
+                      $categories_array = array($new_category_ID);
+                      wp_set_post_categories($postID, $categories_array);
+                    }
+                  }
+
+
+                  array_push($messages, _e('<p>Post created: '  . $title . '</p>', 'audio-to-song-post'));
+                } else {
+                  array_push($messages, _e('<p>Post already exists: ' . $title . '</p>', 'audio-to-song-post'));
+                }
+              } else {
+                array_push($messages, _e('Title not set in the ID3 information.   Make sure it is set for v1 and v2.', 'audio-to-song-post'));
+              }
+        }
   }
-  while ($i <= $limit):
-
-    // Analyze file and store returned data in $ThisFileInfo
-    $filePath = $mp3Files[$i];
-    $ThisFileInfo = $getID3->analyze($filePath);
-
-
-    // TODOS
-    // check to see if it has idv2 tags if so use them
-    // if not use id3v1 tags
-    // for itunes purchases, make sure to use other field names
-    // allow user to insert songs / playlist into existing post!
-
-    $title = $ThisFileInfo['tags_html']['id3v2']['title'][0];
-    $album = $ThisFileInfo['tags_html']['id3v2']['album'][0];
-
-    if ($posting_mode == '2') {
-      $title = $album;
-    } else {
-      $title = $title;
-    }
-
-
-
-    $category = $ThisFileInfo['tags_html']['id3v2']['genre'][0];
-    $description = $ThisFileInfo['tags_html']['id3v2']['subtitle'][0];
-    $bpm = $ThisFileInfo['tags_html']['id3v2']['bpm'][0];
-    $composer = $ThisFileInfo['tags_html']['id3v2']['composer'][0];
-    $grouping = $ThisFileInfo['tags_html']['id3v2']['content_group_description'][0];
-    $album_artist = $ThisFileInfo['tags_html']['id3v2']['band'][0];
-    $encoded_by = $ThisFileInfo['tags_html']['id3v2']['encoded_by'][0];
-
-    $comment0 = $ThisFileInfo['tags_html']['id3v2']['comments'][0];
-    $comment1 = $ThisFileInfo['tags_html']['id3v2']['comments'][1];
-    $comment2 = $ThisFileInfo['tags_html']['id3v2']['comments'][2];
-    $comment3 = $ThisFileInfo['tags_html']['id3v2']['comments'][3];
-
-    if (testcommentsforvalid($comment0)) {
-      $comment = $comment0;
-    } elseif (testcommentsforvalid($comment1)) {
-      $comment = $comment1;
-    } elseif (testcommentsforvalid($comment2)) {
-      $comment = $comment2;
-    } elseif (testcommentsforvalid($comment3)) {
-      $comment = $comment3;
-    } else {
-      $comment = "";
-    }
-
-    if ($post_type == 'post') {
-
-        if ($posting_mode == '1') {
-          $playlist_ids = $song_id;
-        } else {
-          $playlist_ids = $list_of_ids;
-        }
-
-        $description = "<p>[playlist ids=" . $playlist_ids . "]</p>" . $description;
-
-    } else {
-      $the_playlist_array = array(
-        'title' => $title,
-        'mp3' => $song_url,
-        'buy_title_a' => '',
-        'buy_icon_a' => 'cloud-download',
-        'buy_link_a' => '',
-        'buy_title_b' => '',
-        'buy_icon_b' => 'cloud-download',
-        'buy_link_b' => '',
-        'buy_title_c' => '',
-        'buy_icon_c' => 'cloud-download',
-        'buy_link_c' => '',
-        'buy_title_d' => '',
-        'buy_icon_d' => 'cloud-download',
-        'buy_link_d' => '',
-      );
-
-      //loop thru all files and push each array into array_final key is numeric placement of track
-
-      $the_playlist_array_final = array(
-        '0' => $the_playlist_array,
-      );
-
-      $varplaylistarray = serialize($the_playlist_array_final);
-
-      print_r($varplaylistarray);
-      echo "<hr>";
-
-
-    }
-
-
-
-
-
-    // check if we have a title
-    if ($title){
-
-      // check if post exists by search for one with the same title
-      // filtering by song name not working
-      $searchArgs = array(
-        'post_title_like' => $title,
-        'post_type_like' => $post_type,
-      );
-      $titleSearchResult = new WP_Query($searchArgs);
-
-      // If there are no posts with the title of the mp3 then make the post
-      if ($titleSearchResult->post_count == 0) {
-        // create basic post with info from ID3 details
-        $my_post = array(
-          'post_title' => $title,
-          'post_content' => $description,
-          'post_author' => 1,
-          'post_name' => $title,
-        );
-        // Insert the post!!
-        $postID = wp_insert_post($my_post);
-
-        // set post type
-        set_post_type($postID, $post_type);
-
-        if ($post_type == 'songs') {
-          // TODO set artist for songs posts
-          add_post_meta($postID, "playlist", $the_playlist_array_final);
-        }
-
-        //set post tags
-        wp_set_post_tags($postID, $comment);
-
-        //set featured image
-        set_post_thumbnail($postID, $post_thumbnail_id);
-
-        // If the category/genre is set then update the post
-        if(!empty($category)){
-          $category_ID = get_cat_ID($category);
-          // if a category exists
-          if($category_ID) {
-            $categories_array = array($category_ID);
-            wp_set_post_categories($postID, $categories_array);
-          }
-          // if it doesn't exist then create a new category
-          else {
-            $new_category_ID = wp_create_category($category);
-            $categories_array = array($new_category_ID);
-            wp_set_post_categories($postID, $categories_array);
-          }
-        }
-
-
-        array_push($messages, _e('<p>Post created: '  . $title . '</p>', 'audio-to-song-post'));
-      } else {
-        array_push($messages, _e('<p>Post already exists: ' . $title . '</p>', 'audio-to-song-post'));
-      }
-    } else {
-      array_push($messages, _e('Either the title or comments are not set in the ID3 information.   Make sure they are both set for v1 and v2.', 'audio-to-song-post'));
-    }
-    $i++;
-  endwhile; //
 
   // return the messages
   return $messages;
